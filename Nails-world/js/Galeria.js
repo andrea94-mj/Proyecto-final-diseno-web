cf03897615e8a8422646b40370471d1fd17cb067
Andrea Miguel Jarillo <andreamj94@gmail.com>
2024-07-15 21:47:28 +0200

    lightbox terminado!!!!!

--- Nails-world/js/Galeria.js
+++ Nails-world/js/Galeria.js
@@ -1,3 +1,19 @@
 'use strict'
 
 
+const galeriaImgs = document.querySelectorAll('.Galeria-img')
+const lightbox = document.querySelector('.Lightbox')
+const lightboxBtn = document.querySelector('.Lightbox-btn')
+const lightboxImg = document.querySelector('.Imagen-grande')
+
+galeriaImgs.forEach((eachImg) => {
+    eachImg.addEventListener('click', () => {
+        lightbox.style.display = 'flex';
+        lightboxImg.src = eachImg.src;
+    });
+});
+
+lightboxBtn.addEventListener('click', () => {
+    lightbox.style.display = 'none';
+});
+
